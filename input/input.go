2c6f9c9360cab10b94a7430e698d6064e9f0334e
rthorpeii <robertthorpe.ii@gmail.com>
2020-12-01 22:02:17 -0500

    Cleaned up code for Day1 and started a package for processing input files

---
+++ input/input.go
@@ -0,0 +1,42 @@
+package input
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+// ReadInput takes an input file and converts it into a string
+func ReadInput(file string) string {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return ""
+	}
+	return string(data)
+}
+
+// Slice parses the input into slice
+func Slice(input string) []string {
+	return strings.Split(input, "\n")
+}
+
+// Map turns the input into a map
+func Map(input string) map[int]bool {
+	inputSlice := Slice(input)
+
+	var inputMap map[int]bool
+	inputMap = make(map[int]bool)
+
+	for _, num := range inputSlice {
+		number, err := strconv.Atoi(num)
+		if err != nil {
+			log.Fatal("Failed to convert input to map", err)
+		}
+		inputMap[number] = true
+	}
+
+	return inputMap
+}
