6b23a4efd9b3094a1eb7840ccc23337890f435de
rthorpeii <robertthorpe.ii@gmail.com>
2020-12-02 00:35:40 -0500

    Creating Template Files for future days

---
+++ templates/puzzle.go
@@ -0,0 +1,20 @@
+// Package dayXX has solutions for Day XX of Advent of Code
+// https://adventofcode.com/2020/day/XX
+package dayXX
+
+var inputFile string = "dayXX/input.txt"
+var testFile string = "dayXX/testInput.txt"
+
+// PartOne finds
+func PartOne() int {
+	// rawInput := input.ReadInput(testFile)
+
+	return -1
+}
+
+// PartTwo finds
+func PartTwo() int {
+	// rawInput := input.ReadInput(testFile)
+
+	return -1
+}
