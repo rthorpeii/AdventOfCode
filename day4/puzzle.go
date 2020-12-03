748e56b394eaaf547e28dc3bfee334de7efe1060
rthorpeii <robertthorpe.ii@gmail.com>
2020-12-03 18:56:33 -0500

    Initialize directory for day4

---
+++ day4/puzzle.go
@@ -0,0 +1,20 @@
+// Package day4 has solutions for Day 4 of Advent of Code
+// https://adventofcode.com/2020/day/4
+package day4
+
+var inputFile string = "day4/input.txt"
+var testFile string = "day4/testInput.txt"
+
+// PartOne finds
+func PartOne() int {
+	// rawInput := input.ReadInput(testFile)
+
+	return -1
+}
+
+// PartTwo finds
+func PartTwo() int {
+	// rawInput := input.ReadInput(testFile)
+
+	return -1
+}
