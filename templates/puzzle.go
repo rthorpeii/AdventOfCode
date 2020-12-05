4c2e9cedd407ed3cc4a099b3ebe8aada96d9b40a
rthorpeii <robertthorpe.ii@gmail.com>
2020-12-05 12:11:18 -0500

    Updating starting template to print output for both parts

--- templates/puzzle.go
+++ templates/puzzle.go
@@ -2,19 +2,29 @@
 // https://adventofcode.com/2020/day/XX
 package dayXX
 
+import "fmt"
+
 var inputFile string = "dayXX/input.txt"
 var testFile string = "dayXX/testInput.txt"
 
+// SolvePuzzle prints the output produced by running the input and test files on both parts
+func SolvePuzzle() {
+	fmt.Printf("Part 1 - Test: %v \n", PartOne(testFile))
+	fmt.Printf("Part 1 - Actual: %v \n", PartOne(inputFile))
+	fmt.Printf("Part 2 - Test: %v \n", PartTwo(testFile))
+	fmt.Printf("Part 2 - Actual: %v \n", PartTwo(inputFile))
+}
+
 // PartOne finds
-func PartOne() int {
-	// rawInput := input.ReadInput(testFile)
+func PartOne(file string) int {
+	// rawInput := input.ReadInput(file)
 
 	return -1
 }
 
 // PartTwo finds
-func PartTwo() int {
-	// rawInput := input.ReadInput(testFile)
+func PartTwo(file string) int {
+	// rawInput := input.ReadInput(file string)
 
 	return -1
 }
