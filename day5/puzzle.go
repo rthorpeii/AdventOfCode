b4adf769e5a60cce6e7d62199d9067d69ebc3452
rthorpeii <robertthorpe.ii@gmail.com>
2020-12-05 13:17:45 -0500

    Cleaned up code for Day5

--- day5/puzzle.go
+++ day5/puzzle.go
@@ -5,74 +5,72 @@ package day5
 import (
 	"fmt"
 
-	"../input"
+	"github.com/rthorpeii/AdventOfCode2020/input"
 )
 
 var inputFile string = "day5/input.txt"
 var testFile string = "day5/testInput.txt"
 
-// PartOne finds
-func PartOne() int {
-	rawInput := input.Slice(inputFile)
-
-	max := 0
-	for _, seat := range rawInput {
-		minRow := 0
-		maxRow := 127
-		minCol := 0
-		maxCol := 7
-		for _, char := range seat {
-			value := string(char)
-			fmt.Println(value)
-			if value == "F" {
-				maxRow -= ((maxRow - minRow + 1) / 2)
-			} else if value == "B" {
-				minRow += ((maxRow - minRow + 1) / 2)
-			} else if value == "L" {
-				maxCol -= ((maxCol - minCol + 1) / 2)
-			} else if value == "R" {
-				minCol += ((maxCol - minCol + 1) / 2)
-			}
-		}
-		id := minRow*8 + minCol
-		fmt.Println(minRow)
-		fmt.Println(minCol)
-		if id > max {
-			max = id
-		}
-	}
-	return max
+// SolvePuzzle runs the inputs
+func SolvePuzzle() {
+	fmt.Printf("Part 1 - Test: %v \n", PartOne(testFile))
+	fmt.Printf("Part 1 - Actual: %v \n", PartOne(inputFile))
+	fmt.Printf("Part 2 - Test: %v \n", PartTwo(testFile))
+	fmt.Printf("Part 2 - Actual: %v \n", PartTwo(inputFile))
 }
 
-// PartTwo finds
-func PartTwo() int {
-	rawInput := input.Slice(inputFile)
+// findIDs converts the seat string to its id. Each seat is written
+// as a binary partition of the plane e.g. "FBFBBFFRLR"
+// where F means "front", B means "back", L means "left", and R means "right".
+func findIDs(seats *[]string) map[int]bool {
+	ids := make(map[int]bool)
+	for _, seat := range *seats {
+		minRow, maxRow := 0, 128
+		minCol, maxCol := 0, 8
 
-	seats := make(map[int]bool)
-	for _, seat := range rawInput {
-		minRow := 0
-		maxRow := 127
-		minCol := 0
-		maxCol := 7
 		for _, char := range seat {
 			value := string(char)
-			if value == "F" {
-				maxRow -= ((maxRow - minRow + 1) / 2)
-			} else if value == "B" {
-				minRow += ((maxRow - minRow + 1) / 2)
-			} else if value == "L" {
-				maxCol -= ((maxCol - minCol + 1) / 2)
-			} else if value == "R" {
-				minCol += ((maxCol - minCol + 1) / 2)
+			switch value {
+			case "F":
+				maxRow -= ((maxRow - minRow) / 2)
+			case "B":
+				minRow += ((maxRow - minRow) / 2)
+			case "L":
+				maxCol -= ((maxCol - minCol) / 2)
+			case "R":
+				minCol += ((maxCol - minCol) / 2)
 			}
 		}
 		id := minRow*8 + minCol
-		seats[id] = true
+		ids[id] = true
 	}
-	for key := range seats {
-		if seats[key+2] && !seats[key+1] {
+	return ids
+}
+
+// PartOne finds the max id in the input of seats
+func PartOne(file string) int {
+	rawInput := input.Slice(file)
+	ids := findIDs(&rawInput)
+
+	maxID := -1
+	for id := range ids {
+		if id > maxID {
+			maxID = id
+		}
+	}
+
+	return maxID
+}
+
+// PartTwo finds which id is missing from the file, but has both neighboring ids
+func PartTwo(file string) int {
+	rawInput := input.Slice(file)
+	ids := findIDs(&rawInput)
+
+	for key := range ids {
+		if !ids[key+1] && ids[key+2] {
 			return key + 1
-		} else if seats[key-2] && !seats[key-1] {
+		} else if !ids[key-1] && ids[key-2] {
 			return key - 1
 		}
 	}
