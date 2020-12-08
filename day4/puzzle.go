e08dce83c7166a54f7425e75060df651106aeff7
rthorpeii <robertthorpe.ii@gmail.com>
2020-12-07 23:05:22 -0500

    Further cleanup of day4

--- day4/puzzle.go
+++ day4/puzzle.go
@@ -75,47 +75,32 @@ func validatePassport(passport map[string](string)) bool {
 		if value == "" && field != "cid" {
 			return false
 		}
+		var matched bool
 		switch field {
+		case "cid":
+			continue
 		case "byr": // byr (Birth Year) - four digits; at least 1920 and at most 2002.
-			matched, _ := regexp.MatchString(`^(19[2-9][0-9]|200[0-2])$`, value)
-			if !matched {
-				return false
-			}
+			matched, _ = regexp.MatchString(`^(19[2-9][0-9]|200[0-2])$`, value)
 		case "iyr": // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-			matched, _ := regexp.MatchString(`^(201[0-9]|2020)$`, value)
-			if !matched {
-				return false
-			}
+			matched, _ = regexp.MatchString(`^(201[0-9]|2020)$`, value)
 		case "eyr": // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-			matched, _ := regexp.MatchString(`^(202[0-9]|2030)$`, value)
-			if !matched {
-				return false
-			}
-
+			matched, _ = regexp.MatchString(`^(202[0-9]|2030)$`, value)
 		case "hgt":
 			// hgt (Height) - a number followed by either cm or in:
 			// If cm, the number must be at least 150 and at most 193.
 			// If in, the number must be at least 59 and at most 76.
 			matchedCm, _ := regexp.MatchString(`^(1[5-8][0-9]|19[0-3])cm$`, value)
 			matchedIn, _ := regexp.MatchString(`^(59|6[0-9]|7[0-6])in$`, value)
-			if !matchedCm && !matchedIn {
-				return false
-			}
+			matched = matchedCm || matchedIn
 		case "hcl": // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-			matched, _ := regexp.MatchString(`^#[0-9a-f]{6}$`, value)
-			if !matched {
-				return false
-			}
+			matched, _ = regexp.MatchString(`^#[0-9a-f]{6}$`, value)
 		case "ecl": // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-			matched, _ := regexp.MatchString(`^amb|blu|brn|gry|grn|hzl|oth$`, value)
-			if !matched {
-				return false
-			}
+			matched, _ = regexp.MatchString(`^amb|blu|brn|gry|grn|hzl|oth$`, value)
 		case "pid": // pid (Passport ID) - a nine-digit number, including leading zeroes.
-			matched, _ := regexp.MatchString(`^[0-9]{9}$`, value)
-			if !matched {
-				return false
-			}
+			matched, _ = regexp.MatchString(`^[0-9]{9}$`, value)
+		}
+		if !matched {
+			return false
 		}
 	}
 	return true
