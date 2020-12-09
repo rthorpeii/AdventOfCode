482d39751a8d9cc030f1b94d4abc0ab4b2a094ad
rthorpeii <robertthorpe.ii@gmail.com>
2020-12-08 21:35:48 -0500

    Last bit of cleanup for Day 8

--- day8/puzzle.go
+++ day8/puzzle.go
@@ -4,6 +4,7 @@ package day8
 
 import (
 	"fmt"
+	"log"
 	"strings"
 
 	"github.com/rthorpeii/AdventOfCode2020/vm"
@@ -20,14 +21,16 @@ func SolvePuzzle() {
 	fmt.Printf("Part 2 - Actual: %v \n", PartTwo(inputFile))
 }
 
-// PartOne finds
+// PartOne finds what value is in the accumulator before it repeats an instruction
 func PartOne(file string) int {
 	vm := vm.NewVM(file)
 	vm.Execute()
 	return vm.Acc
 }
 
-// PartTwo finds
+// PartTwo finds what the value of the accumulator is after the program terminates.
+// Before the program terminates though, we need to fix it by switching either of
+// one jmp with a nop, or one nop with a jmp.
 func PartTwo(file string) int {
 	vm := vm.NewVM(file)
 	for index, line := range vm.Instructions {
@@ -35,6 +38,8 @@ func PartTwo(file string) int {
 		if parts[0] == "acc" {
 			continue
 		}
+
+		// Create a copy of the VM we can edit and exicute with the altered code.
 		copyVM := vm.Copy()
 		if parts[0] == "jmp" {
 			copyVM.Instructions[index] = "nop " + parts[1]
@@ -48,5 +53,7 @@ func PartTwo(file string) int {
 		}
 	}
 
+	// Should not have reached this point
+	log.Fatal("Could not find a valid output to the program")
 	return -1
 }
