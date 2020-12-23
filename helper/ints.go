b5db2334735cb86090c442b015702fac7efbf35d
rthorpeii <robertthorpe.ii@gmail.com>
2020-12-23 02:32:59 -0500

    First pass at day 23

--- helper/ints.go
+++ helper/ints.go
@@ -16,3 +16,22 @@ func MaxInt(x int, y int) int {
 	}
 	return y
 }
+
+// MinInt calculates the max of two ints
+func MinInt(x int, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+//Mod takes the mod of two nums
+func Mod(x int, y int) int {
+	for x <= 0 {
+		x += y
+	}
+	if x == y {
+		return y
+	}
+	return x % y
+}
