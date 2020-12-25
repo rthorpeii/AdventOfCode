353e4301903a707ab18fbb3b33b85911c0d06cff
rthorpeii <robertthorpe.ii@gmail.com>
2020-12-25 00:29:38 -0500

    Minor updates after day25

--- day25/puzzle.go
+++ day25/puzzle.go
@@ -12,48 +12,23 @@ import (
 var puzzleInputFile string = "day25/input.txt"
 var testFile string = "day25/testInput.txt"
 
-// SolvePuzzle prints the output produced by running the input and test files on both parts
-func SolvePuzzle() {
-	fmt.Printf("Part 1 - Test: %v \n", PartOne(testFile))
-	fmt.Printf("Part 1 - Actual: %v \n", PartOne(puzzleInputFile))
-	fmt.Printf("Part 2 - Test: %v \n", PartTwo(testFile))
-	fmt.Printf("Part 2 - Actual: %v \n", PartTwo(puzzleInputFile))
-}
-
 // PartOne finds
 func PartOne(file string) int {
 	rawInput := input.Slice(file)
-
 	cardPubKey, _ := strconv.Atoi(rawInput[0])
 	doorPubKey, _ := strconv.Atoi(rawInput[1])
-
 	cardLoopSize := findLoopSize(7, cardPubKey)
-	doorLoopSize := findLoopSize(7, doorPubKey)
-
-	fmt.Println(cardLoopSize, doorLoopSize)
-
 	return transform(cardLoopSize, doorPubKey)
 }
-
-// PartTwo finds
-func PartTwo(file string) int {
-	// rawInput := input.ReadInput(file string)
-
-	return -1
-}
-
 func findLoopSize(subject int, target int) int {
-	loopSize := 0
-	value := 1
+	loopSize, value := 0, 1
 	for value != target {
 		value *= subject
 		value %= 20201227
 		loopSize++
 	}
-
 	return loopSize
 }
-
 func transform(loopSize int, subject int) int {
 	value := 1
 	for i := 0; i < loopSize; i++ {
@@ -61,4 +36,12 @@ func transform(loopSize int, subject int) int {
 		value %= 20201227
 	}
 	return value
+}
+
+// SolvePuzzle prints the output produced by running the input and test files on both parts
+func SolvePuzzle() {
+	fmt.Printf("Part 1 - Test: %v \n", PartOne(testFile))
+	fmt.Printf("Part 1 - Actual: %v \n", PartOne(puzzleInputFile))
+	fmt.Printf("Part 2 - Test: %v \n", PartTwo(testFile))
+	fmt.Printf("Part 2 - Actual: %v \n", PartTwo(puzzleInputFile))
 }
